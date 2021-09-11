22d1d1d3cc654f831defbc03c6ef0fdc9ca22575
Liam <jackirelandui@gmail.com>
2021-09-11 14:31:04 -0400

    Remove keys

--- utils/coinpayments.ts
+++ utils/coinpayments.ts
@@ -1,11 +1,10 @@
 import Coinpayments from "coinpayments";
 import crypto from "crypto";
 
-const key = "4c3719dd02ff54d7bf1b459adbf83c366fae99827c14de23ef55c29a6e0f56b9"; //process.env.COIN_KEY as string;
-const secret =
-  "2Dd6c6c5D962e97ADBB53c4Dc0dA7fd09feDd4edB9E0f5D64af8C1202e693509"; //process.env.COIN_SECRET as string;
-const merchantID = "29f4ed3d1e43269d0efab1475cf9c08d"; //process.env.COIN_MERCHANT_ID as string;
-const ipnSecret = ")[>uynDpj76RrBC."; //process.env.COIN_IPN_SECRET as string;
+const key = process.env.COIN_KEY as string;
+const secret = process.env.COIN_SECRET as string;
+const merchantID = process.env.COIN_MERCHANT_ID as string;
+const ipnSecret = process.env.COIN_IPN_SECRET as string;
 
 export const client = new Coinpayments({ key, secret });
 
